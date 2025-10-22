a43de12e4f6be09bb39aae1e56b473ca75d92c43
DuyITLOR <153221271+DuyITLOR@users.noreply.github.com>
2025-10-22 18:34:19 +0700

    Complete the read QR code

--- devices/Esp32-Cam/src/main.cpp
+++ devices/Esp32-Cam/src/main.cpp
@@ -1,20 +1,59 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-#define LED_PIN 4 
+#define CAMERA_MODEL_AI_THINKER
+#include "esp_camera.h"
 
-void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, LOW);  
-  Serial.begin(115200);
-  Serial.println("LED Blink Started!");
+#include "ESP32QRCodeReader.h"
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+
+ESP32QRCodeReader reader(CAMERA_MODEL_AI_THINKER);
+QRCodeData qrCodeData;
+
+void printLCD(String text)
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(text);
 }
 
-void loop() {
-  Serial.println("LED ON");
-  digitalWrite(LED_PIN, HIGH);
-  delay(500);
+void setup()
+{
+  Serial.begin(115200);
+  Wire.begin(14, 15); // SCL, SDA
+  lcd.init();
+  lcd.backlight();
 
-  Serial.println("LED OFF");
-  digitalWrite(LED_PIN, LOW);  
-  delay(500);
+
+  reader.setup();
+  reader.beginOnCore(1);
+
+  Serial.println("\n[INFO] QR scanner started.");
+  printLCD("QR Scanner Ready");
+}
+
+void loop()
+{
+  // Nhận dữ liệu QR
+  if (reader.receiveQrCode(&qrCodeData, 300))
+  {
+    if (qrCodeData.valid)
+    {
+      String payload = (const char *)qrCodeData.payload;
+      Serial.print("[INFO] QR Code Detected: ");
+      Serial.println(payload);
+
+      // Hiển thị lên LCD
+      printLCD(payload);
+    }
+    else
+    {
+      Serial.println("[WARN] Invalid QR Code");
+    }
+  }
+
+  delay(200);
 }
