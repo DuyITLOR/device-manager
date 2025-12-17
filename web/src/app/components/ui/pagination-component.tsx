a492d94bb5ff05de3d099b5e0027360c906c1517
KhoaNguyen-HCMUS <anhkhoa515.dev@gmail.com>
2025-12-18 01:33:14 +0700

    feat: ✨ enhance pagination component with ellipsis for improved navigation

--- web/src/app/components/ui/pagination-component.tsx
+++ web/src/app/components/ui/pagination-component.tsx
@@ -1,6 +1,7 @@
 import {
   Pagination,
   PaginationContent,
+  PaginationEllipsis,
   PaginationItem,
   PaginationLink,
   PaginationNext,
@@ -14,6 +15,37 @@ interface UsersPaginationProps {
 }
 
 export default function PaginationComponent({ currentPage, totalPages, onPageChange }: UsersPaginationProps) {
+  const getPageNumbers = () => {
+    const delta = 2; // Số trang trước và sau
+    const pages: (number | string)[] = [];
+
+    pages.push(1);
+
+    // khoảng trang cần hiển thị
+    let start = Math.max(2, currentPage - delta);
+    let end = Math.min(totalPages - 1, currentPage + delta);
+
+    if (start > 2) {
+      pages.push('...');
+    }
+
+    for (let i = start; i <= end; i++) {
+      pages.push(i);
+    }
+
+    if (end < totalPages - 1) {
+      pages.push('...');
+    }
+
+    if (totalPages > 1) {
+      pages.push(totalPages);
+    }
+
+    return pages;
+  };
+
+  if (totalPages <= 1) return null;
+
   return (
     <div className='mt-6'>
       <Pagination>
@@ -25,15 +57,19 @@ export default function PaginationComponent({ currentPage, totalPages, onPageCha
             />
           </PaginationItem>
 
-          {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
-            <PaginationItem key={page}>
-              <PaginationLink
-                onClick={() => onPageChange(page)}
-                isActive={page === currentPage}
-                className='cursor-pointer'
-              >
-                {page}
-              </PaginationLink>
+          {getPageNumbers().map((page, index) => (
+            <PaginationItem key={`${page}-${index}`}>
+              {page === '...' ? (
+                <PaginationEllipsis />
+              ) : (
+                <PaginationLink
+                  onClick={() => onPageChange(page as number)}
+                  isActive={page === currentPage}
+                  className='cursor-pointer'
+                >
+                  {page}
+                </PaginationLink>
+              )}
             </PaginationItem>
           ))}
 
